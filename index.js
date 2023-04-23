9d7dbf48a414905adf3e54eb460638def15a6ff3
Sara Withrow <saralouise1107@gmail.com>
2023-04-23 18:05:03 -0400

    submitted

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+// findMatching function
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  // fuzzyMatch function
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.slice(0, query.length).toLowerCase() === query.toLowerCase());
+  }
+  
+  // matchName function
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
